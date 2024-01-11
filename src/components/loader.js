b6cb352d303071149bbd8ddfe8e8687d57a591fa
gradyUserName <104601897+gradyUserName@users.noreply.github.com>
2024-01-11 17:47:30 -0600

    loader is there, but idk if I'm going to use it

---
+++ src/components/loader.js
@@ -0,0 +1,17 @@
+import React, { useState, useEffect } from "react";
+import { Oval } from "react-loader-spinner";
+
+const Loader = ({ finishLoading }) => {
+    useEffect(() => {
+        const timeout = setTimeout(() => finishLoading(), 2000);
+        return () => clearTimeout(timeout)
+    }, []);
+
+    return (
+        <Oval
+        color="#FFE733"
+        />
+    );
+};
+
+export default Loader;
