5140dc17ff2adab5ed5610b6a84402b2f237e7c8
gradyUserName <104601897+gradyUserName@users.noreply.github.com>
2024-01-25 08:02:27 -0600

    worked on about and set up scrolling

--- src/components/nav.js
+++ src/components/nav.js
@@ -12,6 +12,19 @@ const Nav = () => {
         setScrolledToTop(window.scrollY < 50);
     };
 
+    const scrollToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    };
+
+    const handleLinkClick = (event) => {
+        event.preventDefault();
+        const targetElement = document.querySelector(event.target.getAttribute('href').substring(1));
+        targetElement.scrollIntoView({ behavior: 'smooth' });
+    };
+
     useEffect(() => {
         window.addEventListener('scroll', handleScroll);
 
@@ -22,9 +35,14 @@ const Nav = () => {
 
     return (
         <div className={scrollDirection === 'up' || scrolledToTop === true ? "nav" : "nav-hidden"} id="nav">
-            <Link to="/" aria-label="home">
+            <Link to="/#hero" onClick={scrollToTop} aria-label="home">
                 <Logo />
             </Link>
+            <ul>
+                <li>
+                    <Link to="/#about" onClick={handleLinkClick} aria-label="About">About</Link>
+                </li>
+            </ul>
         </div>
     );
 };
--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -4,35 +4,39 @@ import selfie from "../../images/self.jpg";
 
 const About = () => {
     return (
-        <div className="about">
+        <div className="about" id="about">
             <h2>About Me</h2>
-            <p>
-                Hello! My name is Grady. I am a self-taught coder living in Oklahoma City.
-                I started learning to code in 2021 when I decided to create scripts for 
-                a legacy system at work that was otherwise very complicated, sensitive to 
-                mistypes, and all around user-unfriendly. I always thought that I wasn't 
-                smart enough for coding, but that project started to change the way I thought, 
-                and seeing the scripts in action, and seeing how much time and headache they saved 
-                people, sparked a genuine love of code that continues to this day.
-            </p>
-            <p>
-                In January of 2022, I made a simple New Year's resolution: Spend 30 minutes learning 
-                to code every day. Pretty soon, 30 minutes a day turned into 45 minutes a day, 
-                turned into an hour a day, et cetera. Now, I spend pretty much all of my free 
-                time learning or working on projects in hopes of eventually getting to do this 
-                professionally.
-            </p>
-            <p>
-                Here are a few of the technologies I am most familiar with:
-            </p>
-            <ul>
-                <li>JavaScript (ES6+)</li>
-                <li>React</li>
-                <li>Gatsby</li>
-                <li>Python</li>
-                <li>Express</li>
-                <li>PostgreSQL</li>
-            </ul>
+            <div className="about-main">
+                <p>
+                    Hello! My name is Grady. I am a self-taught coder living in Oklahoma City.
+                    I started learning to code in 2021 when I decided to create scripts for 
+                    a legacy system at work that was otherwise very complicated, sensitive to 
+                    mistypes, and all around user-unfriendly. I always thought that I wasn't 
+                    smart enough for coding, but that project started to change the way I thought, 
+                    and seeing the scripts in action, and seeing how much time and headache they saved 
+                    people, sparked a genuine love of code that continues to this day.
+                </p>
+                <p>
+                    In January of 2022, I made a simple New Year's resolution: Spend 30 minutes learning 
+                    to code every day. Pretty soon, 30 minutes a day turned into 45 minutes a day, 
+                    turned into an hour a day, et cetera. Now, I spend pretty much all of my free 
+                    time learning or working on projects in hopes of eventually getting to do this 
+                    professionally.
+                </p>
+            </div>
+            <div className="about-techs">
+                <p>
+                    Here are a few of the technologies I am most familiar with:
+                </p>
+                <ul>
+                    <li>JavaScript (ES6+)</li>
+                    <li>React</li>
+                    <li>Gatsby</li>
+                    <li>Python</li>
+                    <li>Express</li>
+                    <li>PostgreSQL</li>
+                </ul>
+            </div>
             <img src={selfie} alt="headshot" />
         </div>
     );
