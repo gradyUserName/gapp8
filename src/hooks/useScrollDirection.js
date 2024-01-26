dd5e7dfe13d9708ee6818dee7de2ceb4ae18c429
gradyUserName <104601897+gradyUserName@users.noreply.github.com>
2024-01-26 11:25:51 -0600

    updated useScrollDirection to use useState for ticking

--- src/hooks/useScrollDirection.js
+++ src/hooks/useScrollDirection.js
@@ -2,36 +2,38 @@ import { useState, useEffect } from "react";
 
 const useScrollDirection = ({ initialDirection, thresholdPixels, off }) => {
     const [scrollDir, setScrollDir] = useState(initialDirection);
+    const [ticking, setTicking] = useState(false);
 
     useEffect(() => {
         const threshold = thresholdPixels || 0;
         let lastScrollY = window.scrollY;
-        let ticking = false;
 
         const updateScrollDir = () => {
             const scrollY = window.scrollY;
 
             if (Math.abs(scrollY - lastScrollY) < threshold) {
-                ticking = false;
+                setTicking(false);
                 return;
             }
 
             setScrollDir(scrollY > lastScrollY ? 'down' : 'up');
             lastScrollY = scrollY > 0 ? scrollY : 0;
-            ticking = false;
+            setTicking(false);
         };
 
         const onScroll = () => {
             if (!ticking) {
                 window.requestAnimationFrame(updateScrollDir);
-                ticking = true;
+                setTicking(true);
             }
         };
 
         !off ? window.addEventListener('scroll', onScroll) : setScrollDir(initialDirection);
 
-        return () => window.removeEventListener('scroll', onScroll);
-    }, [initialDirection, thresholdPixels, off]);
+        return () => {
+            window.removeEventListener('scroll', onScroll);
+        };
+    }, [initialDirection, thresholdPixels, off, ticking]);
 
     return scrollDir;
 };
