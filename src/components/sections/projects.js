5500a9d19e4499113b78994a0724582b8ffbe48e
gradyUserName <104601897+gradyUserName@users.noreply.github.com>
2024-01-26 08:25:58 -0600

    start of project styling

--- src/components/sections/projects.js
+++ src/components/sections/projects.js
@@ -1,9 +1,29 @@
 import React from "react";
+import "../../styles/projects.scss";
+import ytdWindow from "../../images/ytd.png";
 
 const Projects = () => {
     return (
-        <div className="projects">
+        <div className="projects" id="projects">
             <h2>Things I've made</h2>
+            <div className="project" id="ytd">
+                <div className="project-info">
+                    <h4>Featured Project</h4>
+                    <h3>YouTube Video Downloader</h3>
+                    <div className="project-description">
+                        <p>
+                            A simple Python application that lets you download 
+                            and save YouTube videos in the form of an MP4.
+                        </p>
+                    </div>
+                    <ul>
+                        <li>Python</li>
+                        <li>Tkinter</li>
+                        <li>Pytube</li>
+                    </ul>
+                </div>
+                <img src={ytdWindow} alt="YouTube Downloader Image" />
+            </div>
         </div>
     );
 };
