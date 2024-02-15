ab35cd58e840867e9196f633bc808b416592cbe9
gradyUserName <104601897+gradyUserName@users.noreply.github.com>
2024-02-15 17:22:50 -0600

    trying to fix 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,13 +4,13 @@ import '../styles/404.scss';
 
 const NotFoundPage = () => {
   return (
-    <main>
+    <div className="not-found">
       <h1>Page not found</h1>
       <p>
         Sorry, we couldn't find the page you were looking for.
       </p>
       <Link to="/" className="home">Go home</Link>
-    </main>
+    </div>
   )
 }
 
