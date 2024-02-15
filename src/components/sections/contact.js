cbb998a9f6b56971182e610c39e75a03cb889079
gradyUserName <104601897+gradyUserName@users.noreply.github.com>
2024-02-15 16:47:42 -0600

    updated 404 page

--- src/components/sections/contact.js
+++ src/components/sections/contact.js
@@ -6,9 +6,9 @@ const Contact = () => {
         <div className="contact" id="contact">
             <h2>Contact</h2>
             <p>
-                I am currently looking for new opportunities, so my inbox is 
-                open for business. If you have any questions, or just want to 
-                say hi, please reach out and I will reply as soon as I can.
+                Have an opportunity for me? Just want to say hi? Either 
+                way, my inbox is open. Please reach out and I will reply 
+                as soon as I can.
             </p>
             <a href="mailto:gleviholder@gmail.com">
                 Say Hello
